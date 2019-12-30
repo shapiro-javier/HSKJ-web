ee1ec2680e891cc89b62d35eee238449768e46e2
rocknegi <rocknegi53@gmail.com>
2019-12-30 16:33:16 +0530

    fix problem with production build part 2

--- src/pages/categories/SIsLovesMe.js
+++ src/pages/categories/SIsLovesMe.js
@@ -1,15 +1,25 @@
 import React, { useState, useEffect } from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
+
 import Layout from '../../components/layout'
 import Loading from '../loading';
-import { JGET } from '../../utils/API';
 import categoriesStyles from "./categories.module.scss"
-import { Link } from 'gatsby';
+
 export default function SisLovesMe({ location }) {
+    const key = useStaticQuery(graphql`
+    query {
+        site {
+            siteMetadata {
+                GO_KEY
+            }
+        }
+    }
+`)
     console.log(location.state)
     const [data, setRes] = useState([])
     const [loading, setLoading] = React.useState(true);
     useEffect(() => {
-        JGET('folder/list', `fld_id=${location.state.ID}`)
+        fetch(`https://gounlimited.to/api/folder/list?key=${key.site.siteMetadata.GO_KEY}&fld_id=${location.state.ID}`)
             .then(response => response.json())
             .then(res => {
                 setRes(res.result.files)
