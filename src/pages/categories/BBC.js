c8cb47d3cf6a2f46058c93df1a0c5ad233cc4f37
rocknegi <rocknegi53@gmail.com>
2020-01-05 01:24:23 +0530

    Finally add thumbnails XD <iframe SRC={e.link.slice(0, 22) + '/embed-' + e.link.slice(23)} style={{ width: '1px', minWidth: '100%'}} FRAMEBORDER='0 ' allowFullScreen />

--- src/pages/categories/BBC.js
+++ src/pages/categories/BBC.js
@@ -33,13 +33,25 @@ export default function BBC({ location }) {
     }
     return (
         <Layout>
+            <h4 className={categoriesStyles.help} style={{ textAlign: 'center' }}>Click on the title of the video to open it in a new window</h4>
             <ol className={categoriesStyles.posts} >
                 {data.map(e => {
                     return (
 
                         <li className={categoriesStyles.post}>
-                            <div onClick={()=>window.open(e.link)}>
-                            <h2>{e.title}</h2>
+                            <div onClick={() => window.open(e.link)}
+                            >
+                                <h2>{e.title}</h2>
+                                <div
+                                    align="center"
+                                >
+                                    <iframe
+                                        SRC={e.link.slice(0, 22) + '/embed-' + e.link.slice(23)}
+                                        style={{ width: '1px', minWidth: '100%' }}
+                                        FRAMEBORDER='0 '
+                                        allowFullScreen
+                                    />
+                                </div>
                             </div>
                         </li>
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ const IndexPage = () => (
     <div className={indexStyles.div}>
       <h1>Supp nibbas</h1>
       <p>Welcome to my new Gatsby site.</p>
+      <p>Mobile friendly.</p>
       <p>Completely Open Source Website. Look at the code here :  <a href="https://github.com/GO-unlimited">GitHub</a>.</p>
       <p>Now go and watch something great :3.</p>
     </div>
